6765859170b6b38e99b8eb2a76b193ee1e068822
M. Ismail Khan <122792379+IsmailBlouch@users.noreply.github.com>
2023-11-10 23:21:30 +0500

    Add files via upload

---
+++ src/app/components/signup/signup.component.ts
@@ -0,0 +1,168 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { CoreService } from 'src/app/services/core/core.service';
+// import { LoginService } from '../../login/login.service';
+// import { AuthService } from '../services/auth.service';
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.scss'],
+})
+
+export class SignupComponent implements OnInit {
+  private baseUrl: string= "https://localhost:7091/api/User/";
+
+  type: string= 'password';
+  isText: boolean= false;
+  eyeIcon:string='fa-eye-slash';
+
+  show= false;
+  user: any = [];
+  loginForm!: FormGroup;
+  signupForm!: FormGroup;
+  emailBool: boolean = false;
+  comfirmPasswordMatchBool: boolean = false;
+  public showPassword!: boolean;
+  public showPasswordOnPress!: boolean;
+
+
+  constructor(private route: Router, private _coreService: CoreService ,
+    private fb: FormBuilder,private authService:AuthService, private http: HttpClient) {}
+
+  signupUsers: any[] = [];
+
+  ngOnInit(): void {
+    this.createSignForm();
+    this.createLoginForm();
+    if(this.authService.isLoggedIn()){
+      this.route.navigate(['main']);
+    }
+  }
+
+  hideShowPass(){
+    this.isText = !this.isText;
+    this.isText ? this.eyeIcon = 'fa-eye' : this.eyeIcon = 'fa-eye-slash';
+    this.isText ? this.type = 'text' : this.type = 'password';
+  }
+
+
+  createSignForm() {
+    this.signupForm = this.fb.group({
+      userName: [null, [Validators.required, Validators.maxLength(30)]],
+      email: [null, [Validators.required, Validators.email]],
+      phone: [null, [Validators.required]],
+      password: ['', [Validators.required,Validators.minLength(8),Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$')]],
+      confirmPassword: [''],
+    });
+  }
+
+  passwordMatch() {
+    if (
+      this.signupForm.value.password != '' &&
+      this.signupForm.value.confirmPassword != ''
+    ) {
+      if (
+        this.signupForm.value.password != this.signupForm.value.confirmPassword
+      ) {
+        this.comfirmPasswordMatchBool = true;
+      } else {
+        this.comfirmPasswordMatchBool = false;
+      }
+    }
+  }
+
+   
+
+  createLoginForm() {
+    this.loginForm = this.fb.group({
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required, Validators.minLength(8)]],
+    });
+  }
+
+  get f() {
+    return this.signupForm.controls;
+  }
+  
+  onSignUp() {
+    if (
+      this.signupForm.value.password !== this.signupForm.value.confirmPassword
+    ) {
+      this.comfirmPasswordMatchBool = true;
+      alert("Invalid Form");
+     }
+      else {
+      this.comfirmPasswordMatchBool = false;
+      if (this.signupForm.valid) {
+      
+        this.authService.signUp(this.signupForm.value)
+          .subscribe({
+            next: (res) => {
+              // alert(res.message)
+              this,this._coreService.openSnackBar(res.message,"done")
+              // console.log(res);
+              this.signupForm.reset();
+              location.reload();
+              // this.displaySuccessMessage(res.message);
+            },
+            error: (err) => {
+              // console.log(err);
+              // alert(err.error.message)
+              this._coreService.openSnackBar(err.error.message, "done")
+              // this.displayErrorMessage(err);
+            },
+          });
+  
+    } 
+  
+      }}
+   
+
+
+  addUser(user: any) {
+    let users: any[] = [];
+    if (localStorage.getItem('Users')) {
+      // const obj = localStorage.getItem('Users');
+      users = JSON.parse(localStorage.getItem('Users') as string);
+      users = [user, ...users];
+    } else {
+      users = [user];
+    }
+    localStorage.setItem('Users', JSON.stringify(users));
+  }
+
+
+
+
+  onLogin() {
+    // let storedData = localStorage.getItem('Users');
+    this.authService.login(this.loginForm.value)
+    .subscribe({
+      next: (res) => {
+        // alert(res.message)
+        this._coreService.openSnackBar(res.message, 'done');
+        this.authService.setToken(res.token);
+        // console.log();
+        
+        this.route.navigate(['/main']);
+  
+  },
+  error: (err)=>{
+    // alert(err?.error.message)
+    this._coreService.openSnackBar(err?.error.message, 'done');
+  }
+});
+  }
+
+  get l() {
+    return this.loginForm.controls;
+  }
+}
